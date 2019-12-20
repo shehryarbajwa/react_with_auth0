5fbd1cb3010351ec4cb5cd551ab73409d37cca04
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-12-20 15:15:46 -0500

    nav bar added

--- react-auth0/src/App.js
+++ react-auth0/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />;
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
